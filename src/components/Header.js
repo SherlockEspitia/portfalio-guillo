68ee2ec63939458c0a6cf8c1f70972b40f19a0c3
Sherlockespitia <claw.1991@gmail.com>
2025-05-31 18:08:17 -0500

    Se crea un componente que acepta componentes svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,38 @@
 import {EmailIcon} from '../icons/Email.js'
 import { TelephoneIcon } from '../icons/Telephone.js'
 
+function InfoIcon({children}){
+    return <span className='pe-1'>
+        {children}
+    </span>
+}
 export default function Header(){
     return (
-    <header className="w-100">
-        <nav className="navbar container-fluid object-fit-contain bg-primary px-4">
-            <div className="d-flex flex-column ml-2 pt-2">
-                <a className="nav-brand text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Sherlock Espitia</a>
-                <p className="mb-0"> <span className='pe-1'><EmailIcon/></span>email</p>
-                <p> <span className='pe-1'><TelephoneIcon/></span>Telefono</p>
-            </div>
+        <header className="w-100">
+            <nav className="navbar container-fluid object-fit-contain bg-primary px-4">
+                <div className="d-flex flex-column ml-2 pt-2">
+                    <a className="nav-brand text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Sherlock Espitia</a>
+                    <p className="mb-0"> <InfoIcon><EmailIcon/></InfoIcon>email</p>
+                    <p> <InfoIcon><TelephoneIcon/></InfoIcon>Telefono</p>
+                </div>
 
-            <div>
-                <ul className="nav justify-content-between">
-                    <li className="nav-item">
-                        <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Acerca de Mi</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Habilidades</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Proyectos</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Recomendaciones</a>
-                    </li>
-                </ul>
-            </div>
-
-        </nav>
-    </header>)
+                <div>
+                    <ul className="nav justify-content-between">
+                        <li className="nav-item">
+                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Acerca de Mi</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Habilidades</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Proyectos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Recomendaciones</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
 }
