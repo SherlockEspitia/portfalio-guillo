be33323b87fe0ef13b64addfca8d31f548a9e156
Sherlockespitia <claw.1991@gmail.com>
2025-06-08 17:46:58 -0500

    Se limpian los comentarios del header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,3 @@
-/*import { EmailIcon } from '../icons/Email.js'
-import { TelephoneIcon } from '../icons/Telephone.js'
-
-function InfoIcon({children}){
-    return <span className='pe-1'>
-        {children}
-    </span>
-}*/
 import { Nav, Navbar } from "react-bootstrap"
 
 export default function Header(){
@@ -23,42 +15,17 @@ export default function Header(){
                         📧example@mail.com
                     </Navbar.Brand>
                 </Nav>
-                <Nav className="w-100 justify-content-end">
-                    <Nav.Link className="text-white" href="#about">Acerca de Mi</Nav.Link>
-                    <Nav.Link className="text-white" href="#skills">Habilidades</Nav.Link>
-                    <Nav.Link className="text-white" href="#projects">Proyectos</Nav.Link>
-                    <Nav.Link className="text-white" href="#recommendations">Recomendaciones</Nav.Link>
-                </Nav>
+                <Navbar.Collapse id="smContent" >
+                    <Nav className="w-100 justify-content-end" smContent>
+                        <Nav.Link className="text-white" href="#about">Acerca de Mi</Nav.Link>
+                        <Nav.Link className="text-white" href="#skills">Habilidades</Nav.Link>
+                        <Nav.Link className="text-white" href="#projects">Proyectos</Nav.Link>
+                        <Nav.Link className="text-white" href="#recommendations">Recomendaciones</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </header>
 
     )
-    /*return (
-        <header className="w-100">
-            <nav className="navbar container-fluid object-fit-contain bg-primary px-4">
-                <div className="d-flex flex-column ml-2 pt-2">
-                    <a className="nav-brand text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Sherlock Espitia</a>
-                    <p className="mb-0"> <InfoIcon><EmailIcon/></InfoIcon>email</p>
-                    <p> <InfoIcon><TelephoneIcon/></InfoIcon>Telefono</p>
-                </div>
-
-                <div>
-                    <ul className="nav justify-content-between">
-                        <li className="nav-item">
-                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Acerca de Mi</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Habilidades</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Proyectos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-black link-underline link-underline-opacity-0 link-opacity-50-hover" href="/">Recomendaciones</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    )*/
+    
 }
